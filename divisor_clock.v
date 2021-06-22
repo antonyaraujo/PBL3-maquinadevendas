c55849bbaaa9bd2791754b2a36c3f91ca5bb56db
Antony Araujo <contato.antonyaraujo@gmail.com>
2021-06-22 01:56:51 -0300

    Correção do clock

--- divisor_clock.v
+++ divisor_clock.v
@@ -10,17 +10,17 @@ module divisor_clock(clk, saida_tempo, saida_multiplex);
 		output saida_tempo, saida_multiplex;
 		
 	// Registradores
-	reg [29:0] contador;
+	reg [25:0] contador;
 	
 	initial contador = 0;
-	assign saida_tempo = contador[25]; // 0,7451 [25] -> bit 26
+	assign saida_tempo = contador[25]; // 0,7451Hz [25] -> bit 26
 	
 	assign saida_multiplex = contador[17]; //  190,73 Hz [17] -> bit 18
 	
 	always @ (posedge clk) begin
 		contador <= contador + 1;
-		if (contador == 29'h3FFFFFFF) begin
-			contador <= 29'h0;
+		if (contador == 26'h3FFFFFF) begin
+			contador <= 26'h0;
 		end		
 	end
 	
